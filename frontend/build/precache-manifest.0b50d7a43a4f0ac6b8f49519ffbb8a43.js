0c70754b1dac8c897d7bfe0d7fd76b5e7a20342a
EdcleyHilario <edcleyhilario@gmail.com>
2024-08-17 13:54:43 -0300

    Update gitignor front

---
+++ frontend/build/precache-manifest.0b50d7a43a4f0ac6b8f49519ffbb8a43.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896fef1c7e6cc54467da9cdbb026b423",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38914c267542e99226e3",
+    "url": "/static/css/2.77e32367.chunk.css"
+  },
+  {
+    "revision": "38914c267542e99226e3",
+    "url": "/static/js/2.f6cd12fb.chunk.js"
+  },
+  {
+    "revision": "ddf085fe3433b2cc7f7a0730b8b084a2",
+    "url": "/static/js/2.f6cd12fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca4511403ee479a98cf0",
+    "url": "/static/js/3.2dede38c.chunk.js"
+  },
+  {
+    "revision": "d98f98f9156b1385986c1cdc16f69da5",
+    "url": "/static/js/3.2dede38c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1518aa0c9c238bad59b",
+    "url": "/static/js/main.3002b8e1.chunk.js"
+  },
+  {
+    "revision": "2409f5bdc151ecb6d918",
+    "url": "/static/js/runtime-main.fa62f01b.js"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "5c788b8d54d3ebf021114528116dfd77",
+    "url": "/static/media/n8n.5c788b8d.png"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "8244f0423cb09d847ac99e7546cb9220",
+    "url": "/static/media/wa-background-dark.8244f042.png"
+  },
+  {
+    "revision": "2675a05168b3a218703c6d72bb336e02",
+    "url": "/static/media/wa-background.2675a051.png"
+  },
+  {
+    "revision": "e5374d73ff43538fdaa16a3f1a980f4b",
+    "url": "/static/media/webhook.e5374d73.png"
+  }
+]);
